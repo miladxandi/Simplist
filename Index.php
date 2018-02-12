a2e35e2abb24b50fee87543b5c95879361177f8c
Milad Xandi <farvahar.milad@hotmail.com>
2018-02-12 06:16:58 +0330

    Big update

--- Index.php
+++ Index.php
@@ -3,4 +3,5 @@
 require 'Core/Requirement/Autoloader.phtml';
 require 'Route/Core/Router.phtml';
 require 'Route/Constant/Main.phtml';
+//require 'Route/Constant/Eloquent.phtml';
 \Route\Core\Router::Register();
