7615d251eef502df00276e6d2c3eb34e27ebb036
Milad Xandi <farvahar.milad@hotmail.com>
2018-02-13 06:26:58 +0330

    public_html

--- Index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'Core/Requirement/Autoloader.phtml';
-require 'Route/Core/Router.phtml';
-require 'Route/Constant/Main.phtml';
-//require 'Route/Constant/Eloquent.phtml';
-\Route\Core\Router::Register();
---
+++ public_html/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require '../Core/Requirement/Autoloader.phtml';
+require '../Route/Core/Router.phtml';
+require '../Route/Constant/Main.phtml';
+//require 'Route/Constant/Eloquent.phtml';
+\Route\Core\Router::Register();
