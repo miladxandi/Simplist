d35b5a8485b13629f25dd464e5a27ec1c01873e0
Milad Xandi <info@miladzandi.ir>
2018-10-22 03:16:16 +0330

    New Composer Commit

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../Core/Requirement/Autoloader.phtml';
-//require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 require '../Route/Core/Router.phtml';
 require '../Route/Constant/Main.phtml';
 require '../public_html/View/Shared/Layouts.phtml';
